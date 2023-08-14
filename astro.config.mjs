e9d1e4ee05c8f7a1b51577621a11aec19ba79d1b
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2023-08-14 14:30:50 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,83 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
+import partytown from '@astrojs/partytown';
+import tasks from "./src/utils/tasks"
+
+import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
+
+import { SITE_CONFIG, ANALYTICS_CONFIG } from './src/utils/config.ts';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const whenExternalScripts = (items = []) =>
+  ANALYTICS_CONFIG.vendors.googleAnalytics.id && ANALYTICS_CONFIG.vendors.googleAnalytics.partytown
+    ? Array.isArray(items)
+      ? items.map((item) => item())
+      : [items()]
+    : [];
+
+export default defineConfig({
+  site: SITE_CONFIG.site,
+  base: SITE_CONFIG.base,
+  trailingSlash: SITE_CONFIG.trailingSlash ? 'always' : 'never',
+
+  output: 'static',
+  compressHTML: true,
+
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+
+    mdx(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+        ri: ['twitter-fill', 'facebook-box-fill', 'linkedin-box-fill', 'whatsapp-fill', 'mail-fill'],
+      },
+    }),
+
+    ...whenExternalScripts(() =>
+      partytown({
+        config: { forward: ['dataLayer.push'] },
+      })
+    ),
+
+    tasks()
+  ],
+
+  markdown: {
+    remarkPlugins: [readingTimeRemarkPlugin],
+  },
+
+  experimental:{
+    assets: true
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
+});
